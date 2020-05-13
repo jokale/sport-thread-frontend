5d73f9d4bbe2dd157a00db57c0b9e5cc5404feea
jokale <kalemajoanna@gmail.com>
2020-05-13 12:38:32 +0100

    render and map comments onto indivudal thread pages

--- src/components/Commentlist.js
+++ src/components/Commentlist.js
@@ -3,9 +3,20 @@ import React from 'react'
 const Commentlist = (props) => {
     return(
         <div>
-            Comments
+            {props.comments && props.comments.map(comment =>
+              <div key={comment.id}> 
+              <h4>User: <h6>{comment.username}</h6></h4> 
+              <h6>Created: {comment.date}</h6> 
+              <h4>Comment {comment.id}:</h4><h5>  {comment.comment_description} </h5>
+
+              </div>  
+            )}
         </div>
     )
 }
 
+
+
 export default Commentlist
+
+
